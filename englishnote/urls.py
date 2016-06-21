fdef5d12607cad3418995e6a217ec6065935714e
insoo <inu0616@gmail.com>
2016-06-21 23:49:46 +0900

    수정3

--- englishnote/urls.py
+++ englishnote/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
     url(r'^randomnote/$', views.shuffle, name='random'),
     url(r'^createnote/$', views.note_new, name='note_new'),
-    url(r'^/(?P<pk>\d+)/delete$', views.note_delete, name="note_delete"),
-    url(r'^/(?P<note_pk>\d+)/edit$', views.note_edit, name="note_edit"),
+    url(r'^(?P<pk>\d+)/delete$', views.note_delete, name="note_delete"),
+    url(r'^(?P<note_pk>\d+)/edit$', views.note_edit, name="note_edit"),
 ]
